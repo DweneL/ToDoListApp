c6e2189d0da5871324bfdc8a4fb551e8dacf24bb
Dwene Louis <Dwenegt@yahoo.com>
2024-09-17 01:37:07 -0400

    First Commit

---
+++ src/main/java/com/TodoListApp/TodoListApp/Random.java
@@ -0,0 +1,4 @@
+package com.TodoListApp.TodoListApp;
+
+public class Random {
+}
---
+++ src/test/java/com/TodoListApp/TodoListApp/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodoListApp.TodoListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
